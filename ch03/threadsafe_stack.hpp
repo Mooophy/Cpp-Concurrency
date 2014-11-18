eaa4f809ff845ac7fd1e580b837cacd109d4c681
mooophy <MoophyAlan@gmail.com>
2014-11-19 11:41:55 +1300

    implemented, not commented nor tested yet. 	modified:   threadsafe_stack.hpp

--- ch03/threadsafe_stack.hpp
+++ ch03/threadsafe_stack.hpp
@@ -1,3 +1,8 @@
+//!
+//! @author Yue Wang
+//! @date   19.11.2014
+//!
+
 #ifndef THREADSAFE_STACK_HPP
 #define THREADSAFE_STACK_HPP
 
@@ -21,9 +26,10 @@ struct EmptyStack : std::exception
 template<typename Value>
 class ThreadsafeStack
 {
-    using Guard = std::lock_guard<std::mutex>;
+    using Guard         =   std::lock_guard<std::mutex>;
 public:
-    //! defautl Ctor
+    using SharedPointer =   std::shared_ptr<Value>;
+
     ThreadsafeStack(){}
 
     //! copy Ctor
@@ -35,13 +41,44 @@ public:
 
     ThreadsafeStack& operator = (const ThreadsafeStack&) = delete;
 
-    void push(Value new_val);
-    std::shared_ptr<Value> pop();
-    void pop(Value& val);
-    bool empty()const;
+    void push(Value new_val)
+    {
+        Guard g{mutex_};
+        data_.push(new_val);
+    }
+
+    SharedPointer pop()
+    {
+        Guard g{mutex_};
+        throw_exception_if_empty();
+        const auto pointer = std::make_shared<Value>(data_.top());
+        data_.pop();
+
+        return pointer;
+    }
+
+    void pop(Value& dest)
+    {
+        Guard g{mutex_};
+        throw_exception_if_empty();
+        dest = data_.top();
+        data_.pop();
+    }
+
+    bool empty()const
+    {
+        Guard g{mutex_};
+        return data_.empty();
+    }
+
 private:
     std::stack<Value> data_;
     mutable std::mutex mutex_;
+
+    void throw_exception_if_empty()
+    {
+        if(data_.empty())   throw para::EmptyStack{};
+    }
 
 };
 }//namespace
