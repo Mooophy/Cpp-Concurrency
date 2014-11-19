2b422e352fe562a0d0d32b6b4d54010566254fbf
mooophy <MoophyAlan@gmail.com>
2014-11-19 14:59:43 +1300

    commented 	modified:   ch03/threadsafe_stack.hpp 	modified:   ch03/threadsafe_stack_test.cpp

--- ch03/threadsafe_stack.hpp
+++ ch03/threadsafe_stack.hpp
@@ -23,18 +23,22 @@ namespace para {
 class EmptyStackException : std::exception
 {
 public:
+    //! default Ctor
     EmptyStackException():
         msg_{"para exception : empty stack"}
     {}
 
+    //! Ctor
     explicit EmptyStackException(const char* msg):
         msg_{msg}
     {}
 
+    //! Ctor
     explicit EmptyStackException(const std::string& msg):
         msg_{msg.c_str()}
     {}
 
+    //! move Ctor
     explicit EmptyStackException(std::string&& msg)noexcept:
         msg_{std::move(msg).c_str()}
     {}
@@ -56,6 +60,14 @@ private:
     const std::string msg_;
 };
 
+/**
+ * @brief   ThreadsafeStack
+ * @note    based on Listing 3.4
+ * @modifications
+ *      1. alias for std::lock_guard<std::mutex>
+ *      2. alias for std::shared_ptr<Value>
+ *      3. a wrapper for exception throwing
+ */
 template<typename Value>
 class ThreadsafeStack
 {
@@ -74,12 +86,14 @@ public:
 
     ThreadsafeStack& operator = (const ThreadsafeStack&) = delete;
 
+    //! push
     void push(Value new_val)
     {
         Guard g{mutex_};
         data_.push(new_val);
     }
 
+    //! pop returnning shared pointer
     SharedPointer pop()
     {
         Guard g{mutex_};
@@ -90,6 +104,7 @@ public:
         return pointer;
     }
 
+    //! pop with reference as argument
     void pop(Value& dest)
     {
         Guard g{mutex_};
@@ -98,6 +113,7 @@ public:
         data_.pop();
     }
 
+    //! empty
     bool empty()const
     {
         Guard g{mutex_};
@@ -108,11 +124,11 @@ private:
     std::stack<Value> data_;
     mutable std::mutex mutex_;
 
+    //! wrapper for exception throwing
     void throw_exception_if_empty()
     {
         if(data_.empty())   throw para::EmptyStackException{};
     }
-
 };
 }//namespace
 #endif // THREADSAFE_STACK_HPP
--- ch03/threadsafe_stack_test.cpp
+++ ch03/threadsafe_stack_test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "threadsafe_stack.hpp"
+#include <thread>
 
 /**
  * @brief test for para::EmptyStackException
  *
- * @output para's exception was thrown
+ * @output
+ *          para's exception was thrown
  */
 void test_para_excetion() throw()
 {
@@ -19,5 +21,45 @@ void test_para_excetion() throw()
 
 int main()
 {
+    para::ThreadsafeStack<int> stk;
+    auto init = [&]{
+        for(auto&& i : {1,2,3,4,5,6,7,8})   stk.push(i);
+    };
 
+    std::thread init_stk
+    {
+        [&]{
+            init();
+        }
+    };
+
+    std::thread pop_with_pointer_n_print
+    {
+        [&]{
+            while(!stk.empty())
+                std::cout << *stk.pop() << " " << std::flush;
+        }
+    };
+
+    std::thread pop_with_value_n_print
+    {
+        [&]{
+            int buff=0;
+            while(!stk.empty())
+            {
+                stk.pop(buff);
+                std::cout << buff << " " << std::flush;
+            }
+        }
+    };
+
+    init_stk.join();
+    pop_with_pointer_n_print.join();
+    pop_with_value_n_print.join();
+
+    return 0;
 }
+//! output
+//! since the three threads have not been synchronized,
+//! multiple types of outputs are expected.
+//!
