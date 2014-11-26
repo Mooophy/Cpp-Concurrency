a2539dd67c5caf452a0f1dfc4d7551f1a90e0b9c
mooophy <MoophyAlan@gmail.com>
2014-11-26 16:23:14 +1300

    new file:   functional_quick_sort.cpp

---
+++ ch04/functional_quick_sort.cpp
@@ -0,0 +1,41 @@
+#include <../include/utilities.hpp>
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+namespace para {
+
+template<typename T>
+std::list<T> sequential_functional_quick_sort(std::list<T> input)
+{
+    if(input.empty())   return input;   //  termination condtion
+
+    std::list<T> ret;
+    ret.splice(ret.begin(),input,input.begin());
+    T pivot = ret.front();
+    auto divide_point = std::partition(input.begin(),input.end(), [=](T const& t){
+        return t < pivot;
+    });
+
+    std::list<T> lower;
+    lower.splice(lower.end(), input, input.begin(),divide_point);
+
+    auto new_lower (para::sequential_functional_quick_sort(lower));     //recur
+    auto new_higher(para::sequential_functional_quick_sort(input));     //recur
+
+    ret.splice(ret.end(), new_higher);
+    ret.splice(ret.begin(),new_lower);
+    return ret;
+}
+}//namespace
+
+int main()
+{
+    std::list<int> l {6,3,2,5,1,4};
+    para::println_range(l);
+
+    l = para::sequential_functional_quick_sort(l);
+    para::println_range(l);
+
+    return 0;
+}
--- include/utilities.hpp
+++ include/utilities.hpp
@@ -10,17 +10,24 @@
 
 namespace para{
 
+template<typename Range>
+inline std::ostream& println_range(Range const& rng)
+{
+    std::cout << "para> ";
+    for(auto elem : rng)    std::cout << elem << " ";
+    return std::cout << std::endl;
+}
 
 template<typename Printable>
 inline std::ostream& println(Printable const& to_print)
 {
-    return std::cout << to_print << std::endl;
+    return std::cout << "para>" << to_print << std::endl;
 }
 
 template<typename Printable>
 inline std::ostream& println(Printable && to_print)
 {
-    return std::cout << std::move(to_print) << std::endl;
+    return std::cout << "para>" << std::move(to_print) << std::endl;
 }
 
 }//namespace
